a0ce066d66434e261a4bccf3357054c43070fbe6
ming <read1516@naver.com>
2019-10-10 05:43:40 +0100

    0.1v (test)

---
+++ precache-manifest.f3f432492fdc5c3bcd0b32601cc3e50d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50081347dcc535b73011c8b81a1e86a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57467fb8d237631a9347",
+    "url": "/static/css/main.a72e7dbb.chunk.css"
+  },
+  {
+    "revision": "7896a07321073f1c4422",
+    "url": "/static/js/2.8c5f3e29.chunk.js"
+  },
+  {
+    "revision": "57467fb8d237631a9347",
+    "url": "/static/js/main.9013466f.chunk.js"
+  },
+  {
+    "revision": "dc3e713a84b75459fa08",
+    "url": "/static/js/runtime-main.441a2df4.js"
+  }
+]);
